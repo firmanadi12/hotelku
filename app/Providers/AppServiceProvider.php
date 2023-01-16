cfbca701fe38a69cc22c8c500aa5204ff24b45c2
Adi Firmansyah <38484738+firmanadi12@users.noreply.github.com>
2023-01-16 20:21:12 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,9 +5,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-// use App\Models\Page;
-// use App\Models\Room;
-// use App\Models\Setting;
+use App\Models\Page;
+use App\Models\Room;
+use App\Models\Setting;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -31,12 +31,12 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
-//         $page_data = Page::where('id',1)->first();
-//         $room_data = Room::get();
-//         $setting_data = Setting::where('id',1)->first();
+        $page_data = Page::where('id',1)->first();
+        $room_data = Room::get();
+        $setting_data = Setting::where('id',1)->first();
 
-//         view()->share('global_page_data', $page_data);
-//         view()->share('global_room_data', $room_data);
-//         view()->share('global_setting_data', $setting_data);
+        view()->share('global_page_data', $page_data);
+        view()->share('global_room_data', $room_data);
+        view()->share('global_setting_data', $setting_data);
     }
 }
